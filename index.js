8bda1bd331a64ead70e0fe590247865fed009bb2
Naveen bathal <naveenbathal@Naveens-MacBook-Air.local>
2019-06-11 21:56:49 -0400

    Main

---
+++ index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+console.log(process.env.secretOne);
+console.log(process.env.secretTwo);
+console.log(process.env.secretThree);
+console.log(process.env.secretFour);
+console.log(process.env.secretFive);
---
+++ index2.js
@@ -0,0 +1,5 @@
+console.log(process.env.SecretOne);
+console.log(process.env.SecretTwo);
+console.log(process.env.SecretThree);
+console.log(process.env.SecretFour);
+console.log(process.env.SecretFive);
